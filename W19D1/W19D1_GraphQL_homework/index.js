c88fa5be451579df88980443781520598df39f89
Abby Xu <abby.tsui2016@gmail.com>
2019-11-03 22:09:02 -0800

    update homework

--- W19D1/W19D1_GraphQL_homework/index.js
+++ W19D1/W19D1_GraphQL_homework/index.js
@@ -1,6 +1,27 @@
+const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
+const db = require('./config/keys').mongoURI;
+const expressGraphQL = require('express-graphql')
+const bodyParser = require('body-parser')
 
-app.get("/", (req, res) => res.send("Hello World"));
+mongoose
+	// .connect(db)
+	.connect(db, { useNewUrlParser: true })
+	.then(() => console.log("Connected to MongoDB successfully"))
+	.catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("mhello"));
+
+//make form data available in req.body
+app.use(bodyParser.json());
+
+app.use(
+	"/graphql",
+	expressGraphQL({
+		graphiql: true
+	})
+);
+
 
 app.listen(5000, () => console.log('Server is running on port 5000'));
