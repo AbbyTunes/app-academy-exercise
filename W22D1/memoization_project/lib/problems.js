c36161ed38099f0311b34480d44d9f4ea1bfab1c
Abby Xu <abby.tsui2016@gmail.com>
2019-11-27 08:28:38 -0800

    Updated memoization project

--- W22D1/memoization_project/lib/problems.js
+++ W22D1/memoization_project/lib/problems.js
@@ -46,7 +46,7 @@ function minChange(coins, amount, memo = {}) {
 	if (amount in memo) return memo[amount];
 
 	let num_coins = []
-	// minChange([1, 2, 5], 11)         // => 3, because 5 + 5 + 1 = 11
+	
 	coins.forEach(coin => {
 		if (coin <= amount) {
 			num_coins.push(minChange(coins, amount - coin, memo) + 1);
@@ -56,92 +56,6 @@ function minChange(coins, amount, memo = {}) {
 	memo[amount] = Math.min(...num_coins);
 	return memo[amount];
 }
-
-// minChange([1, 2, 5], 11)         // => 3, because 5 + 5 + 1 = 11
-// minChange([1, 4, 5], 8))         // => 2, because 4 + 4 = 8
-// minChange([1, 5, 10, 25], 15)    // => 2, because 10 + 5 = 15
-// minChange([1, 5, 10, 25], 100)   // => 4, because 25 + 25 + 25 + 25 = 100
-
-// function minChange(coins, amount, memo = {}) {
-// 	if (amount === 0) {
-// 		return 0;
-// 	}
-
-// 	if (amount in memo) {
-// 		return memo[amount];
-// 	}
-
-// 	let num_coins = [];
-// 	for (let i = coins.length - 1; i >= 0; --i) {
-// 		let newAmount = amount - coins[i];
-// 		if (newAmount < 0) {
-// 			continue;
-// 		}
-
-// 		num_coins.push(minChange(coins, newAmount, memo) + 1);
-// 	}
-
-// 	memo[amount] = Math.min(...num_coins);
-//  	return memo[amount];
-// }
-
-
-// minChange([1, 2, 5], 11)         // => 3, because 5 + 5 + 1 = 11
-	let numbers = 0;
-
-function minChange(coins, amount, memo = {}) {
-	if (amount === 0) {
-		console.log(`final numbers ${numbers}`);
-		return 0;
-	}
-
-	if (amount in memo) {
-		return memo[amount];
-	}
-
-	for (let i = coins.length - 1; i >= 0; i--) {
-		let remaining = amount - coins[i];
-		if (remaining >= 0) {
-			console.log(`remaining ${remaining}`);
-			memo[amount] = minChange(coins, remaining, memo) + 1;
-		} else {
-			continue;
-		}
-	}
-	return memo[amount];
-}
-		
-
-// var change = function (amount, coins, memo = {}) {
-
-// 	// run a key instead of a hard count
-// 	let count = amount + "-" + coins
-
-// 	if (typeof memo[count] === "number") return memo[count];
-
-// 	if (amount === 0) {
-// 		return 1
-// 	}
-// 	if (coins.length === 0) {
-// 		return 0
-// 	}
-
-// 	let changeCounter = 0;
-// 	let firstCoin = coins[0]; // grab current coin
-// 	let tempCounter = 0;
-
-// 	while (amount >= (firstCoin * tempCounter)) {
-// 		let newAmount = amount - (firstCoin * tempCounter);
-// 		changeCounter += change(newAmount, coins.slice(1), memo)
-// 		tempCounter += 1;
-// 	}
-
-// 	memo[count] = changeCounter;
-// 	return memo[count];
-
-
-// };
-		
 
 module.exports = {
     lucasNumberMemo,
